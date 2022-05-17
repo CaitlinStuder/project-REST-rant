410fd5b15199f033e92f59c38d13c05066359778
CaitlinStuder <studercaitlin98@gmail.com>
2022-05-16 20:59:39 -0400

    adding style to rest rant

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // importing router from places.js file
 // first argument '/places' sets all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller
@@ -20,4 +22,3 @@ app.get('*', (req, res) => {
   })  
 
 app.listen(process.env.PORT)
-

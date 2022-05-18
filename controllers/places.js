5a32481f331fe481d3e6f5d930d0b19fa637844b
CaitlinStuder <studercaitlin98@gmail.com>
2022-05-17 22:05:59 -0400

    added functionality to edit

--- controllers/places.js
+++ controllers/places.js
@@ -19,9 +19,13 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
+
 router.get('/:id', (req, res) => {
   let i = Number(req.params.id)
   if (isNaN(i)) {
@@ -36,7 +40,30 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  let i = Number(req.params.id)
+    if (isNaN(i)) {
+        res.render('error404')
+    }
+    else if (!places[i]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[i]
+        places[i] = req.body
+        res.redirect(`/places/${i}`)
+    }
 })
 
 router.delete('/:id', (req, res) => {
@@ -54,11 +81,22 @@ router.delete('/:id', (req, res) => {
 })
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET /places/:id/edit stub')
+  let i = Number(req.params.id)
+    if (isNaN(i)) {
+        res.render('error404')
+    }
+    else if (!places[i]) {
+        res.render('error404')
+    }
+    else { 
+        res.render('places/edit', { place: places[i], i})
+    }
 })
+
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
 })
+
 router.delete('/:id/rant/:rantId', (req, res) => {
   res.send('GET /places/:id/rant/:rantId stub')
 })

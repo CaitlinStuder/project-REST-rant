81ef89f90a2433a7665136077b0985a55ce3f157
CaitlinStuder <studercaitlin98@gmail.com>
2022-05-17 21:04:22 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,16 +1,6 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-// GET /places
-router.get('/', (req, res) => {
-  res.render('places/index', { places })
-})
-
-router.get('/new', (req, res) => {
-	res.render('places/new')
-  }
-)
-
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
@@ -27,6 +17,26 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+// GET /places
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
+
+router.get('/new', (req, res) => {
+	res.render('places/new')
+  }
+)
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  } else if (!places[id]) {
+      res.render('error404')
+  } else {
+      res.render('places/show', { place: places[id] })
+  }
+})
 
 
 module.exports = router

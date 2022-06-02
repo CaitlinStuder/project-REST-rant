4a94a8fbd9ac68f1caf6a135c178c5788e30338c
CaitlinStuder <studercaitlin98@gmail.com>
2022-06-02 09:35:06 -0400

    rest rant part 10

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,6 +1,25 @@
 const React = require('react')
 const Def = require('../default')
 function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
           <main>
@@ -50,3 +69,6 @@ function show (data) {
     )
 }
 module.exports = show
+
+  
+ 
